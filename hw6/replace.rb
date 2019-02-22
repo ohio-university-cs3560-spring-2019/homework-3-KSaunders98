908fd56019f68ca00b2373130f7f3993af1d5875
KSaunders98 <ks825016@ohio.edu>
2019-02-21 21:43:40 -0500

    Completed HW6, including case insensitivity and preservation

--- hw6/replace.rb
+++ hw6/replace.rb
@@ -1,16 +1,41 @@
 str = STDIN.read
 
-str = str.gsub(/\bF\S+/) {
+#preserves case of characters, including replaced characters
+str = str.gsub(/\b[fF]\S+/) {
 	|m|
-	if m[1] == m[1].upcase then
-		"GH" + m[1, m.length - 1]
+	first = ""
+	if m[0] == "f" then
+		first = "g"
 	else
-		"Gh" + m[1, m.length - 1]
+		first = "G"
 	end
+	second = ""
+	if m[1] == m[1].upcase then
+		second = "H"
+	else
+		second = "h"
+	end
+	first + second + m[1, m.length - 1]
+}
+str = str.gsub(/(\S+)[sS][hH]\b/) {
+	|m|
+	first = ""
+	if m[m.length - 2] == "s" then
+		first = "t"
+	else
+		first = "T"
+	end
+	second = ""
+	if m[m.length - 1] == "h" then
+		second = "i"
+	else
+		second = "I"
+	end
+	m[0, m.length - 2] + first + second
 }
 str = str.gsub(/(\S+)s[hH]\b/, '\1t')
 str = str.gsub(/(\S+)S[Hh]\b/, '\1T')
-str = str.gsub(/(\S+)i(\S+)/, '\1o\2')
-str = str.gsub(/(\S+)I(\S+)/, '\1O\2')
+str = str.gsub(/(?<=\w)i(?=\w)/, 'o')
+str = str.gsub(/(?<=\w)I(?=\w)/, 'O')
 
 puts str
